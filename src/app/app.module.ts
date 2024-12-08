50658794bb04a42cec1bc1e242e2e6052ee31aa7
Anaid0 <vpdiana06@gmail.com>
2024-12-08 00:06:51 -0500

    actualizazao de la actuliazazao DE LA ACTUALIZAZAO

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import {} from '@angular/common/http';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { MaterialModule } from './material.module';
 import { LoginUserComponent } from './pages/login-user/login-user.component';
